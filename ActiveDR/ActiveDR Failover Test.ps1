13ccc85c2787e2aad64825d89756cb4ce2fdf0d5
Andy Yun <SQLBek@gmail.com>
2023-11-28 11:02:28 -0500

    Add files via upload
    Added additional edits from Anthony Nocentino's feedback

--- ActiveDR/ActiveDR Failover Test.ps1
+++ ActiveDR/ActiveDR Failover Test.ps1
@@ -3,12 +3,12 @@
 # 
 # Author: Andy Yun
 # Written: 2023-05-10
-# Updated: 2023-10-09
+# Updated: 2023-11-28
 #
 # Scenario: 
 # Test failover only in DR.  Do not impact Production
 # 
-# Single test database "CookbookDemo_ADR" on two RDM volumes: 
+# Single test database "ActiveDR_Example_DB" on two RDM volumes: 
 # data & log, on each SQL Server. 
 #
 # Prerequisites:
@@ -40,11 +40,11 @@ Import-Module PureStoragePowerShellSDK2
 # Set Variables
 $ArrayName = "sn1-x70-f06-27.puretec.purestorage.com"   # DR FlashArray
 $PodName = "ayun-sql03-activedr-remote"                 # Pod name on the DR FlashArray
-$TargetVM = "ayun-sql19-04"                             # DR Virtual Machine
+$TargetSQLServer= "ayun-sql19-04"                       # DR SQL Server
 
-# Connect to DR VM
-Write-Host "Connecting to DR VM..." -ForegroundColor Red
-$TargetVMSession = New-PSSession -ComputerName $TargetVM 
+# Connect to DR SQL Server
+Write-Host "Connecting to DR Host..." -ForegroundColor Red
+$TargetSQLServerSession = New-PSSession -ComputerName $TargetSQLServer
 
 # Connect to DR FlashArray
 Write-Host "Connecting to DR FlashArray..." -ForegroundColor Red
@@ -73,46 +73,37 @@ Break   # Safety-net to prevent accidental "run everything" of this script
 # serial number to properly identify which disks to manipulate.
 # Therefore, loop here until serial numbers we expect manifest
 #
-# Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk  }
-$DiskOne = Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' }  }
-$DiskTwo = Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' }  }
+# Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk  }
+$DiskOne = Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' }  }
+$DiskTwo = Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' }  }
 while (($DiskOne -eq $null) -or ($DiskTwo -eq $null)) {
     Write-Host "Waiting for DR volumes to come online" -ForegroundColor Red
-    Invoke-Command -Session $TargetVMSession -ScriptBlock { Update-HostStorageCache }
-    $DiskOne = Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' }  }
-    $DiskTwo = Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' }  }
+    Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Update-HostStorageCache }
+    $DiskOne = Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' }  }
+    $DiskTwo = Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' }  }
 }
 
 # Online the windows disks 
 # NOTE: Change disk serial number(s) to appropriate disks
 Write-Host "Onlining DR volumes..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsOffline $False }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsOffline $False }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsOffline $False }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsOffline $False }
 
 # Setting volumes to Read/Write
 # NOTE: Change disk serial number(s) to appropriate disks
 Write-Host "Setting Read/Write for DR volumes..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsReadOnly $False }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsReadOnly $False }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsReadOnly $False }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsReadOnly $False }
 
 # Confirm state of disks
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk }    
-Break   # Safety-net to prevent accidental "run everything" of this script
-
-
-# Start SQL Server instance (if applicable)
-Write-Host "Starting SQL Server Instance Services..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Start-Service MSSQLSERVER; Start-Service SQLSERVERAGENT}
-
-Write-Host "Confirm SQL Server Instance is running..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Service -name "MSSQLSERVER" }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Format-Table }
 Break   # Safety-net to prevent accidental "run everything" of this script
 
 
 # Online the database
-Write-Host "Onlining database CookbookDemo_ADR in DR..." -ForegroundColor Red
-$Query = "ALTER DATABASE CookbookDemo_ADR SET ONLINE WITH ROLLBACK IMMEDIATE"
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Param($querytask) Invoke-Sqlcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
+Write-Host "Onlining database ActiveDR_Example_DB in DR..." -ForegroundColor Red
+$Query = "ALTER DATABASE ActiveDR_Example_DB SET ONLINE WITH ROLLBACK IMMEDIATE"
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock {Param($querytask) Invoke-Sqlcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
 
 # Force script termination to avoid accidental "run entire script"
 Write-Host "DR Pod Promotion completed.  Go check SQL Server." -ForegroundColor Red
@@ -125,16 +116,16 @@ Exit   # Safety-net to prevent accidental "run everything" of this script
 
 # Offline the database
 Write-Host "Offlining the database(s)..." -ForegroundColor Red
-$Query = "ALTER DATABASE CookbookDemo_ADR SET OFFLINE WITH ROLLBACK IMMEDIATE"
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Param($querytask) Invoke-Sqlcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
+$Query = "ALTER DATABASE ActiveDR_Example_DB SET OFFLINE WITH ROLLBACK IMMEDIATE"
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock {Param($querytask) Invoke-Sqlcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
 
 # Offline the volume
 Write-Host "Offlining the DR volume(s)..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsOffline $True }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsOffline $True }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsOffline $True }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsOffline $True }
 
 # Confirm state of disks
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Format-Table }
 Break   # Safety-net to prevent accidental "run everything" of this script
 
 # Demote DR Pod
--- ActiveDR/ActiveDR Full Failover.ps1
+++ ActiveDR/ActiveDR Full Failover.ps1
@@ -3,12 +3,12 @@
 # 
 # Author: Andy Yun
 # Written: 2023-05-10
-# Updated: 2023-10-09
+# Updated: 2023-11-28
 #
 # Scenario: 
 # Manually failover Production to DR.  Then fully failback.
 # 
-# Single test database "CookbookDemo_ADR" on two RDM volumes: 
+# Single test database "ActiveDR_Example_DB" on two RDM volumes: 
 # data & log, on each SQL Server. 
 #
 # Prerequisites:
@@ -38,12 +38,12 @@ Import-Module PureStoragePowerShellSDK2
 
 # Set Variables
 $ArrayName = "sn1-m70-f06-33.puretec.purestorage.com"   # Production FlashArray
-$PodName = "ayun-sql03-activedr"                        # Pod name on the Production FlashArray
-$TargetVM = "ayun-sql19-03"                             # Production Virtual Machine
+$PodName = "ayun-sql03-activedr"                        # Pod name on the DR FlashArray
+$SourceSQLServer = "ayun-sql19-03"                      # Production SQL Server
 
 # Connect to Production VM
 Write-Host "Connecting to Production VM..." -ForegroundColor Red
-$TargetVMSession = New-PSSession -ComputerName $TargetVM 
+$SourceSQLServerSession = New-PSSession -ComputerName $SourceSQLServer 
 
 # Connect to FlashArray
 Write-Host "Connecting to Production FlashArray..." -ForegroundColor Red
@@ -53,23 +53,23 @@ Break   # Safety-net to prevent accidental "run everything" of this script
 
 
 # Offline the database
-Write-Host "Offlining the database(s)..." -ForegroundColor Red
-$Query = "ALTER DATABASE CookbookDemo_ADR SET OFFLINE WITH ROLLBACK IMMEDIATE"
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Param($querytask) Invoke-Sqlcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
+Write-Host "Offlining the Production database(s)..." -ForegroundColor Red
+$Query = "ALTER DATABASE ActiveDR_Example_DB SET OFFLINE WITH ROLLBACK IMMEDIATE"
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock {Param($querytask) Invoke-SQLcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
 
 # Offline the volume
 Write-Host "Offlining the Production volume(s)..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88B' } | Set-Disk -IsOffline $True }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88C' } | Set-Disk -IsOffline $True }
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88B' } | Set-Disk -IsOffline $True }
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88C' } | Set-Disk -IsOffline $True }
 
 # Confirm state of disks
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk }
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock { Get-Disk | Format-Table }
 Break   # Safety-net to prevent accidental "run everything" of this script
 
 
 # Demote Production Pod with Quiesce
 Write-Host "Demoting Production Pod..." -ForegroundColor Red
-Update-Pfa2Pod -Array $FlashArray -Name $PodName -Quiesce -RequestedPromotionState "demoted"
+Update-Pfa2Pod -Array $FlashArray -Name $PodName -Quiesce $True -RequestedPromotionState "demoted"
 
 # Confirm Production Pod status
 Get-Pfa2Pod -Array $FlashArray -Name $PodName
@@ -89,11 +89,11 @@ Import-Module PureStoragePowerShellSDK2
 # Set Variables
 $ArrayName = "sn1-x70-f06-27.puretec.purestorage.com"   # DR FlashArray
 $PodName = "ayun-sql03-activedr-remote"                 # Pod name on the DR FlashArray
-$TargetVM = "ayun-sql19-04"                             # DR Virtual Machine
+$TargetSQLServer = "ayun-sql19-04"                      # DR SQL Server
 
 # Connect to DR VM
 Write-Host "Connecting to DR VM..." -ForegroundColor Red
-$TargetVMSession = New-PSSession -ComputerName $TargetVM 
+$TargetSQLServerSession = New-PSSession -ComputerName $TargetSQLServer 
 
 # Connect to FlashArray
 Write-Host "Connecting to DR FlashArray..." -ForegroundColor Red
@@ -122,46 +122,36 @@ Break   # Safety-net to prevent accidental "run everything" of this script
 # Because disk serial number can change between reboots, need to programmatically reference
 # serial number to properly identify which disks to manipulate.
 # Therefore, loop here until serial numbers we expect manifest
-$DiskOne = Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' }  }
-$DiskTwo = Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' }  }
+$DiskOne = Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' }  }
+$DiskTwo = Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' }  }
 while (($DiskOne -eq $null) -or ($DiskTwo -eq $null)) {
     Write-Host "Waiting for DR volumes to come online" -ForegroundColor Red
-    Invoke-Command -Session $TargetVMSession -ScriptBlock { Update-HostStorageCache }
-    $DiskOne = Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' }  }
-    $DiskTwo = Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' }  }
+    Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Update-HostStorageCache }
+    $DiskOne = Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' }  }
+    $DiskTwo = Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' }  }
 }
 
 # Online the windows disks 
 # NOTE: Change disk serial number(s) to appropriate disks
 Write-Host "Onlining DR volumes..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsOffline $False }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsOffline $False }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsOffline $False }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsOffline $False }
 
 # Setting volumes to Read/Write
 # NOTE: Change disk serial number(s) to appropriate disks
 Write-Host "Setting Read/Write for DR volumes..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsReadOnly $False }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsReadOnly $False }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsReadOnly $False }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsReadOnly $False }
 
 # Confirm state of disks
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk }
-Break   # Safety-net to prevent accidental "run everything" of this script
-
-
-# Start SQL Server instance (if applicable)
-# Prequisite = databases have previously been attached
-Write-Host "Starting SQL Server Instance Services..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Start-Service MSSQLSERVER; Start-Service SQLSERVERAGENT}
-
-Write-Host "Confirm SQL Server Instance is running..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Service -name "MSSQLSERVER" }
+Invoke-Command -Session $TargetSqlServerSession -ScriptBlock { Get-Disk | Format-Table }
 Break   # Safety-net to prevent accidental "run everything" of this script
 
 
 # Online the database
-Write-Host "Onlining database CookbookDemo_ADR in DR..." -ForegroundColor Red
-$Query = "ALTER DATABASE CookbookDemo_ADR SET ONLINE WITH ROLLBACK IMMEDIATE"
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Param($querytask) Invoke-Sqlcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
+Write-Host "Onlining database ActiveDR_Example_DB in DR..." -ForegroundColor Red
+$Query = "ALTER DATABASE ActiveDR_Example_DB SET ONLINE WITH ROLLBACK IMMEDIATE;"
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock {Param($querytask) Invoke-SQLcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
 
 # Force script termination to avoid accidental "run entire script"
 Write-Host "DR Pod Promotion completed." -ForegroundColor Red
@@ -178,11 +168,11 @@ Import-Module PureStoragePowerShellSDK2
 # Set Variables
 $ArrayName = "sn1-x70-f06-27.puretec.purestorage.com"   # DR FlashArray
 $PodName = "ayun-sql03-activedr-remote"                 # Pod name on the DR FlashArray
-$TargetVM = "ayun-sql19-04"                             # DR Virtual Machine
+$TargetSQLServer = "ayun-sql19-04"                      # DR SQL Server
 
 # Connect to DR VM
 Write-Host "Connecting to DR VM..." -ForegroundColor Red
-$TargetVMSession = New-PSSession -ComputerName $TargetVM 
+$TargetSQLServerSession = New-PSSession -ComputerName $TargetSQLServer 
 
 # Connect to FlashArray
 Write-Host "Connecting to DR FlashArray..." -ForegroundColor Red
@@ -193,25 +183,25 @@ Break   # Safety-net to prevent accidental "run everything" of this script
 
 # Offline the database
 Write-Host "Offlining the database(s)..." -ForegroundColor Red
-$Query = "ALTER DATABASE CookbookDemo_ADR SET OFFLINE WITH ROLLBACK IMMEDIATE"
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Param($querytask) Invoke-Sqlcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
+$Query = "ALTER DATABASE ActiveDR_Example_DB SET OFFLINE WITH ROLLBACK IMMEDIATE;"
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock {Param($querytask) Invoke-SQLcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
 
 # Offline the volume
 Write-Host "Offlining the DR volume(s)..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsOffline $True }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsOffline $True }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835C' } | Set-Disk -IsOffline $True }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq '81F096D1C1642A69017A835B' } | Set-Disk -IsOffline $True }
 
 # Confirm state of disks
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk }
+Invoke-Command -Session $TargetSQLServerSession -ScriptBlock { Get-Disk | Format-Table}
 Break   # Safety-net to prevent accidental "run everything" of this script
 
 
 # Demote Production Pod with Quiesce
 Write-Host "Demoting DR Pod..." -ForegroundColor Red
-Update-Pfa2Pod -Array $FlashArray -Name $PodName -Quiesce -RequestedPromotionState "demoted"
+Update-Pfa2Pod -Array $FlashArray -Name $PodName -Quiesce $true -RequestedPromotionState "demoted"
 
 # Confirm Production Pod status
-Get-PfaPods -Array $FlashArray | Where-Object {$_.Name -eq $PodName}
+Get-Pfa2Pod -Array $FlashArray | Where-Object {$_.Name -eq $PodName}
 
 # Force script termination to avoid accidental "run entire script"
 Write-Host "DR Pod Demotion completed." -ForegroundColor Red
@@ -220,12 +210,12 @@ Break   # Safety-net to prevent accidental "run everything" of this script
 
 # Set Variables
 $ArrayName = "sn1-m70-f06-33.puretec.purestorage.com"   # Production FlashArray
-$PodName = "ayun-sql03-activedr"                        # Pod name on the Production FlashArray
-$TargetVM = "ayun-sql19-03"     
+$PodName = "ayun-sql03-activedr"                        # Pod name on the DR FlashArray
+$SourceSQLServer = "ayun-sql19-03"                      # Production SQL Server  
 
 # Connect to Production VM
 Write-Host "Connecting to Production VM..." -ForegroundColor Red
-$TargetVMSession = New-PSSession -ComputerName $TargetVM 
+$SourceSQLServerSession = New-PSSession -ComputerName $SourceSQLServer 
 
 # Connect to FlashArray
 Write-Host "Connecting to Production FlashArray..." -ForegroundColor Red
@@ -253,29 +243,23 @@ Break   # Safety-net to prevent accidental "run everything" of this script
 # Online the windows disks 
 # NOTE: Change disk serial number(s) to appropriate disks
 Write-Host "Onlining Production volumes..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88B' } | Set-Disk -IsOffline $False }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88C' } | Set-Disk -IsOffline $False }
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88B' } | Set-Disk -IsOffline $False }
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88C' } | Set-Disk -IsOffline $False }
 
 # Setting volumes to Read/Write
 # NOTE: Change disk serial number(s) to appropriate disks
 Write-Host "Setting Read/Write for Production volumes..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88B' } | Set-Disk -IsReadOnly $False }
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88C' } | Set-Disk -IsReadOnly $False }
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88B' } | Set-Disk -IsReadOnly $False }
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock { Get-Disk | Where-Object { $_.SerialNumber -eq 'AC5FC11F8B3B49A00178C88C' } | Set-Disk -IsReadOnly $False }
 
 # Confirm state of disks
-Invoke-Command -Session $TargetVMSession -ScriptBlock { Get-Disk }
+Invoke-Command -Session $SourceSqlServerSession -ScriptBlock { Get-Disk | Format-Table }
 Break   # Safety-net to prevent accidental "run everything" of this script
 
-
-# Start SQL Server instance (if applicable)
-# Prequisite = databases have previously been attached
-Write-Host "Starting SQL Server Instance Services..." -ForegroundColor Red
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Start-Service MSSQLSERVER; Start-Service SQLSERVERAGENT}
-
 # Online the database
-Write-Host "Onlining database CookbookDemo_ADR in Production..." -ForegroundColor Red
-$Query = "ALTER DATABASE CookbookDemo_ADR SET ONLINE WITH ROLLBACK IMMEDIATE"
-Invoke-Command -Session $TargetVMSession -ScriptBlock {Param($querytask) Invoke-Sqlcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
+Write-Host "Onlining database ActiveDR_Example_DB in Production..." -ForegroundColor Red
+$Query = "ALTER DATABASE ActiveDR_Example_DB SET ONLINE WITH ROLLBACK IMMEDIATE;"
+Invoke-Command -Session $SourceSQLServerSession -ScriptBlock {Param($querytask) Invoke-SQLcmd -ServerInstance . -Database master -Query $querytask} -ArgumentList ($Query)
 
 # Force script termination to avoid accidental "run entire script"
 Write-Host "Production Pod Promotion completed." -ForegroundColor Red
