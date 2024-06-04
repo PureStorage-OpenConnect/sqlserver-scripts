131d9c406725d5fb26454d03d0e64fb6f033a7ea
rquimbey <48227137+rquimbey@users.noreply.github.com>
2024-06-04 10:55:13 -0700

    Add examples with the backupsdk
    Would like to start showing examples using the backupsdk

---
+++ demos-backupsdk/Protection Group Database Refresh/Protection Group Database Refresh vVol BackupSDK.ps1
@@ -0,0 +1,79 @@
+##############################################################################################################################
+# Protection Group Database Refresh - vVol
+#
+# Scenario: 
+#    This script will refresh a database on the target server from a source database on a different server.  This script
+#    utilizes a FlashArray Protection Group, to snapshot and clone two volumes simultaneously.  
+#
+# Prerequisities:
+#    1. Two SQL Server instances with a single database, whose data file(s) are contained within 1 volume and log file(s)
+#       are contained within a 2nd volume.  
+#    2. A Protection Group defined with the two volumes (data and log) as members
+# 
+# Usage Notes:
+#    This simple example assumes there is only one database residing on two different volumes (data & log).  If multiple 
+#    databases are present, additional code must be added to offline/online all databases present on the affected volumes 
+#    in the Protection Group.  Also note that the Protection Group use may include other volumes without negative impact.  
+#    Any extraneous volumes will simply not be utilized during the cloning step.  
+# 
+# Disclaimer:
+#    This example script is provided AS-IS and meant to be a building block to be adapted to fit an individual 
+#    organization's infrastructure.
+##############################################################################################################################
+
+
+
+# Import powershell modules
+Import-Module PureStorage.FlashArray.Backup
+
+
+
+# Declare variables
+$SourceSQLServer          = 'SqlServer1'                           # Name of source SQL Server
+$TargetSQLServer          = 'SqlServer2'                           # Name of target SQL Server
+$SourceArrayName          = 'flasharray1.example.com'              # Source FlashArray FQDN
+$TargetArrayName          = 'flasharray2.example.com'              # Target FlashArray FQDN
+$DatabaseName             = 'ExampleDb1'                           # Name of the database being snapshotted & cloned
+$ProtectionGroupName      = 'SqlServer1_Pg'                        # Protection Group name in the FlashArray
+$VolumeSet                = 'volset1'                              # Name of the Volume Set
+$SourcePath               = 's:\'                                  # Path of Volumes to Snapshot
+$TargetPath               = 'n:\'                                  # Path of Volumes to Mount
+$VolumeType               = 'physical'                             # Physical, vVol, or pRDM
+$vCenterAddress           = 'vcenter.example.com'                  # vCenter Server
+$SourceVMName             = 'sqlvm4'                               # Source VM
+$TargetVMName             = 'sqlvm5'                               # Target VM
+
+# Set Credentials - this assumes the same credential for the target SQL Server and the FlashArray. If this is a VMware VM using pRDM or vVol, a vCenter credential is required.
+$FlashArrayCredential = Get-Credential
+$SQLServerCredential = Get-Credential
+$vCenterCredential = Get-Credential
+
+# Offline the target database
+$Query = "ALTER DATABASE [$DatabaseName] SET OFFLINE WITH ROLLBACK IMMEDIATE"
+Invoke-Sqlcmd -ServerInstance $TargetSQLServer -Database master -Query $Query
+
+
+
+# Create a new snapshot of the Protection Group
+$Snapshot = Invoke-PsbSnapshotJob -vcenteraddress $vcenteraddress -VcenterCredential $vcentercredential -vmname $sourceVMName -FlashArrayAddress $ArrayName -FlashArrayCredential $FlashArrayCredential -VolumeSetName $VolumeSet -VolumeType $VolumeType -ComputerAddress $SourceSQLServer -ComputerCredential $SQLServerCredential -Path $SourcePath -pgroupname $ProtectionGroupName -ReplicateNow
+
+
+
+# Find the existing mounted snapshot so it can be dismounted
+$FindMount = Get-PsbSnapshotSetMountHistory -FlashArrayAddress $ArrayName -FlashArrayCredential $FlashArrayCredential | where {($_.Computer -contains $TargetSQLServer -and $_.HistoryId -match $VolumeSet)}
+
+
+
+# Dismount the snapshot
+Dismount-PsbSnapshotSet -flasharrayaddress $ArrayName -flasharraycredential $FlashArrayCredential -mountid $FindMount[0].mountid -computeraddress $TargetSQLServer -computercredential $SQLServerCredential
+
+
+
+# Mount the newer snapshot
+Mount-PsbSnapshotSet -HistoryId $Snapshot.HistoryId -FlashArrayAddress $ArrayName -flasharraycredential $FlashArrayCredential -computeraddress $TargetSQLServer -computercredential $SQLServerCredential -Path $TargetPath -VMName $targetvmname -VCenterAddress $vCenterAddress -VCenterCredential $vCenterCredential
+
+
+
+# Online the database
+$Query = "ALTER DATABASE [$DatabaseName] SET ONLINE WITH ROLLBACK IMMEDIATE"
+Invoke-Sqlcmd -ServerInstance $TargetSQLServer -Database master -Query $Query
---
+++ demos-backupsdk/Protection Group Database Refresh/README.md
@@ -0,0 +1,43 @@
+# Protection Group Database Refresh
+**Protection Group Database Refresh Scripts**
+<p align="center"></p>
+This folder contains an example script to take a Protection Group snapshot of a SQL Server database, whose files are split between two volumes (typically data & log volumes). 
+<BR><BR>
+
+
+**Files:**
+- Protection Group Database Refresh.ps1
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+**Scenario:**
+<BR>This example script shows steps to snapshot a FlashArray Protection Group that contains the data and log volumes of a SQL Server database.  It will then clone those two volumes, from the Protection Group snapshot, and overlay another pre-existing set of volumes on a different, non-production SQL Server.  
+
+All references to a "target" refer to the non-production side.
+
+**Prerequisites:**
+1. Two SQL Server instances with a single database, whose data file(s) are contained within 1 volume and log file(s) are contained within a 2nd volume.  
+2. A Protection Group defined with the two volumes (data and log) as members
+
+**Usage Notes:**
+<BR>This simple example assumes there is only one database residing on two different volumes (data & log).  If multiple databases are present, additional code must be added to offline/online all databases present on the affected volumes in the Protection Group.  Also note that the Protection Group use may include other volumes without negative impact.  Any extraneous volumes will simply not be utilized during the cloning step.  
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+**Disclaimer:**
+<BR>
+This example script is provided AS-IS and meant to be a building block to be adapted to fit an individual organization's infrastructure.
+<BR>
+<BR>
+
+We encourage the modification and expansion of these scripts by the community. Although not necessary, please issue a Pull Request (PR) if you wish to request merging your modified code in to this repository.
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+_The contents of the repository are intended as examples only and should be modified to work in your individual environments. No script examples should be used in a production environment without fully testing them in a development or lab environment. There are no expressed or implied warranties or liability for the use of these example scripts and templates presented by Pure Storage and/or their creators._
